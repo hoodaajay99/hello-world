211252807d43ac6f4458954f476952f53355789f
Ajay Hooda <hoodaajay@gmail.com>
2019-08-29 15:51:10 +0530

    Created hello.c
    Created hello.c

---
+++ intro/hello.c
@@ -0,0 +1,13 @@
+// Include Header files
+#include "stdio.h" //printf, scanf etc
+
+// Start of C application 
+int main() 
+{
+    char var[] = "world";
+    // Print to Console/Terminal
+    printf("Hello %s\n", var);
+
+    // Return Success
+    return 0;
+}
